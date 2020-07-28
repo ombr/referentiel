fb7c905df905a9c0a0442ab3d7d8d7d724d806c6
Luc Boissaye <luc@boissaye.fr>
2020-07-28 16:11:44 -0400

    Rework test matrix

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,4 @@
 var customLaunchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    version: '36'
-  },
   sl_chrome_latest: {
     base: 'SauceLabs',
     browserName: 'chrome'
@@ -11,11 +6,6 @@ var customLaunchers = {
   sl_firefox_latest: {
     base: 'SauceLabs',
     browserName: 'firefox'
-  },
-  sl_firefox_16: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '16'
   },
   sl_ie_latest: {
     base: 'SauceLabs',
@@ -30,19 +20,9 @@ var customLaunchers = {
     base: 'SauceLabs',
     browserName: 'Safari'
   },
-  sl_safari: {
-    base: 'SauceLabs',
-    browserName: 'Safari',
-    version: '9'
-  },
   sl_edge_latest: {
     base: 'SauceLabs',
     browserName: 'Microsoftedge'
-  },
-  sl_edge: {
-    base: 'SauceLabs',
-    browserName: 'Microsoftedge',
-    version: '13'
   },
   sl_android_latest: {
     base: 'SauceLabs',
@@ -50,61 +30,19 @@ var customLaunchers = {
     platform: 'Android',
     deviceName: 'Android Emulator'
   },
-  sl_android: {
-    base: 'SauceLabs',
-    browserName: 'Browser',
-    platform: 'Android',
-    version: '5.0',
-    deviceName: 'Android Emulator'
-  },
   sl_ios_safari_latest: {
     base: 'SauceLabs',
     browserName: 'iphone',
     platform: 'OS X 10.9'
   },
-  sl_ios_safari: {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.9',
-    version: '9.0'
-  },
-  bs_opera_latest: {
-    base: 'BrowserStack',
-    browser: 'opera',
-    os: 'Windows',
-    os_version: '8'
-  },
-  bs_opera: {
-    base: 'BrowserStack',
-    browser: 'opera',
-    os: 'Windows',
-    os_version: '8',
-    browser_version: '23'
-  }
+  // bs_opera_latest: {
+  //   base: 'BrowserStack',
+  //   browser: 'opera',
+  //   os: 'Windows',
+  //   os_version: '8'
+  // },
 };
 
-// bs_chrome_windows:
-//   base: 'BrowserStack',
-//   os: 'Windows',
-//   os_version: '10',
-//   browser: 'chrome',
-//   browser_version: '47.0'
-// bs_firefox_windows:
-//   base: 'BrowserStack',
-//   os: 'Windows',
-//   os_version: '10',
-//   browser: 'firefox',
-// bs_firefox_mac:
-//   base: 'BrowserStack',
-//   browser: 'firefox',
-//   browser_version: '21.0',
-//   os: 'OS X',
-//   os_version: 'Mountain Lion'
-// bs_iphone5:
-//   base: 'BrowserStack',
-//   device: 'iPhone 5',
-//   os: 'ios',
-//   os_version: '6.0'
 module.exports = function(config) {
   return config.set({
     basePath: '',
