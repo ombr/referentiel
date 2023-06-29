eaa3c725bbeaa3129b9a9e5d2c8b0b2ec5bf2289
ombr <luc@boissaye.fr>
2023-06-29 11:44:44 +0000

    Deploying to docs from @ ombr/referentiel@33d059a34598726834e0d0fee9c1b28e3bc75685 🚀

---
+++ 2.0.1/referentiel.d.ts
@@ -0,0 +1,50 @@
+export declare type Matrix = [
+    [
+        number,
+        number,
+        number
+    ],
+    [
+        number,
+        number,
+        number
+    ],
+    [
+        number,
+        number,
+        number
+    ]
+];
+declare class Referentiel {
+    reference: Node;
+    static jquery?: (element: Node) => {
+        css: (a: string) => string;
+    };
+    offsetParent: Node | null;
+    constructor(reference: Node, offsetParent?: Node | null);
+    static convertPointFromPageToNode(node: Node, point: [number, number]): [number, number];
+    convertPointFromPageToNode(point: [number, number]): [number, number];
+    globalToLocal(point: [number, number]): [number, number];
+    localToGlobal(point: [number, number]): [number, number];
+    static multiplyPoint(m: Matrix, point: [number, number]): [number, number];
+    static exportNumber(v: number): number;
+    _round(value: number): number;
+    matrixInv(): Matrix;
+    matrix(): Matrix;
+    static offsetParent(node: Node): Node | null;
+    matrixLocale(): Matrix;
+    matrixTransform(): Matrix;
+    matrixTransformOrigin(): Matrix;
+    matrixBorder(): Matrix;
+    parent(element: Node): Node | null;
+    matrixOffset(): Matrix;
+    matrixSVGViewbox(): Matrix;
+    offset(element: Node): [number, number];
+    css(property: string): string;
+    static mult(...args: Matrix[]): Matrix;
+    static identity(): Matrix;
+    static det(m: Matrix): number;
+    static inv(m: Matrix): Matrix;
+    static multiply(a: Matrix, b: Matrix): Matrix;
+}
+export { Referentiel };
